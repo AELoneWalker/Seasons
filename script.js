eb2a8d21d05d75877509f1441ad7f257fbb91cb9
Ashutosh-Patnaik <67516845+Ashutosh-Patnaik@users.noreply.github.com>
2020-07-18 18:59:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function summer(){
+    alert("Hey! It is summer");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')"
+     document.body.style.backgroundSize = "100%";
+     document.body.style.backgroundRepeat = "no-repeat"
+}
+function winter(){
+    alert("Hey! It is winter");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')"
+    document.body.style.backgroundSize = "100%;"
+    document.body.style.backgroundRepeat = "no-repeat"
+}
+function autumn(){
+    alert("Hey! It is autumn");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')"
+    document.body.style.backgroundSize = "100%;"
+    document.body.style.backgroundRepeat = "no-repeat"
+}
+function spring(){
+    alert("Hey! It is spring");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')"
+    document.body.style.backgroundSize = "100%;"
+    document.body.style.backgroundRepeat = "no-repeat"
+}
+function copy(){
+    alert("LOL! You can't copy!");
+}
